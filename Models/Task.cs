ddabfda923450fa5c3f90fceb67a76752dc73bf6
anjoyank <60146761+anjoyank@users.noreply.github.com>
2021-04-05 17:33:05 -0400

    push this

---
+++ Models/Task.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AirplaneAPI.Models
+{
+    public class Task {
+        public int ItemNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime LogDate { get; set; }
+        public int LogHours { get; set; }
+        public int IntervalMonths { get; set; }
+        public int IntervalHours { get; set; }
+
+    }
+}
