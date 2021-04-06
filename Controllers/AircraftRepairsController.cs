f5664e020964c0c549bd6937b4b72333ce36bf8c
anjoyank <60146761+anjoyank@users.noreply.github.com>
2021-04-06 10:30:01 -0400

    morning start

--- Controllers/AircraftRepairsController.cs
+++ Controllers/AircraftRepairsController.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using AirplaneAPI.Models;
+using AirplaneAPI.Data;
 namespace AirplaneAPI.Controllers
 {
+    //should inherit from controller class if using View component
     [Route ("/aircraft/{aircraftId}/duelist")]
     [ApiController]
     public class AircraftRepairsController : ControllerBase
     {
-        public ActionResult <> 
+        private readonly MockAircraftRepairRepo _repository = new MockAircraftRepairRepo();
+        [HttpPost]
+        public ActionResult <IEnumerable<Task>> ComputeTasksDue(int aircraftId) 
+        {
+
+        }
     }
 }
--- Data/MockAircraftRepairRepo.cs
+++ Data/MockAircraftRepairRepo.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+using AirplaneAPI.Models;
+
 namespace AirplaneAPI.Data
 {
     public class MockAircraftRepo : IAircraftRepairRepo
     {
+                IEnumerable<Aircraft> GetAircraftById(int id);
 
     }
 }
